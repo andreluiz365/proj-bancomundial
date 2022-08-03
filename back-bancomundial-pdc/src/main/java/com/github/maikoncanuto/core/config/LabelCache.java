6926eed8c12c5c89d9632ad4b5a15dd0ab278ac5
Andre Luiz Silva <andre.silva@redbelt.com.br>
2022-08-02 23:22:27 -0300

    add: proj mvn

---
+++ back-bancomundial-pdc/src/main/java/com/github/maikoncanuto/core/config/LabelCache.java
@@ -0,0 +1,12 @@
+package com.github.maikoncanuto.core.config;
+
+public class LabelCache {
+
+    private LabelCache() {
+    }
+
+    public static final String CACHE_COUNTRIES_WORLD_BANK = "cache-countries-world-bank";
+    public static final String CACHE_INDICATORS_WORLD_BANK = "cache-indicators-world-bank";
+
+
+}
